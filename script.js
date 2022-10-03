6ed8aba14b817b9b8ecc27806069479fd50b2db0
Jennifer Inskip <jennifer.m.inskip@gmail.com>
2022-10-03 17:53:06 -0400

    JS update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let yourName = prompt("Welcome to GC mini golf!  What is your name?");
 let holeCount = prompt("Welcome " + yourName + "!  Would you like to play 3 or 6 holes?");
+while (holeCount !== 3 || holeCount !== 6) {
+    holeCount = prompt("Please, tell me, would you like to play 3 or 6 holes?");
+    if (holeCount == 3 || holeCount == 6) break;
+}
 
 let total = 0;
-
 if (holeCount == 3) {
     par = 9;
 }
@@ -14,6 +17,8 @@ for (let i = 0; i < holeCount; i++) {
 }
 let score = total-par;
 
+//End Results console.log
+
 if (score < 0) {
     console.log("Great job, " + yourName + "!  Your total par was: " + score + ".");
 }
@@ -23,10 +28,3 @@ if (score == 0) {
 if (score > 0) {
     console.log("Nice try, " + yourName + "...  Your total par was: +" + score + ".");
 }
-
-
-
-
-/*If even with par, the console log reads: "Good game, (user’s name). Your total par was: 0"
-If under par, the console log reads: "Great job, (user’s name)! Your total par was: -(par)"
-If over par, the console log reads: "Nice try, (user’s name)... Your total par was: +(par)"*/
